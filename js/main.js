c156fcd88368055134b0e885df7e649f3118c664
Robby Horsley <robbyhorsley@Robbys-MacBook-Pro.local>
2018-04-06 10:51:28 -0600

    update styles

--- js/main.js
+++ js/main.js
@@ -68,79 +68,24 @@ Vue.component('language-list', {
   },
   template: '<ul><li v-for="skill in skills"><img :src="skill.img" /></li></ul>',
 })
+Vue.component('about-me', {
+  data: function() {
+    return {
+      message: 'You want to know about me?',
+    }
+  },
+  template: '<p>{{message}}</p>',
+})
 new Vue({
   el: '#app',
   // data: {
   //   message: 'Hello Vue!',
-  //   skills:[
-  //   	{
-  //       "id": 0,
-  //   	  "name": "javascript",
-  //       "img": "/img/javascript.png",
-  //       "component": "javascript"	
-  //   	},
-  //     {
-  //       "id": 1,
-  //       "name": "html",
-  //       "img": "/img/html5.png",
-  //       "component": "html",
-  //     },
-  //     {
-  //       "id" : 2,
-  //       "name": "css",
-  //       "img": "/img/css3.png",
-  //       "component": "css",
-  //     },
-  //     {
-  //       "id" : 3,
-  //       "name": "node",
-  //       "img": "/img/node.png",
-  //       "component": "node",
-  //     },
-  //     {
-  //       "id" : 4,
-  //       "name": "vue",
-  //       "img": "/img/vue.png",
-  //       "component": "vue",
-  //     },
-  //     {
-  //       "id" : 5,
-  //       "name": "react",
-  //       "img": "/img/react.png",
-  //       "component": "react",
-  //     },
-  //     {
-  //       "id" : 6,
-  //       "name": "php",
-  //       "img": "/img/php.png",
-  //       "component": "php",
-  //     },
-  //     {
-  //       "id" : 7,
-  //       "name": "cakephp",
-  //       "img": "/img/cakephp.png",
-  //       "component": "php",
-  //     },
-  //     {
-  //       "id" : 8,
-  //       "name": "angular",
-  //       "img": "/img/angular.png",
-  //       "component": "angular",
-  //     },
-  //     {
-  //       "id" : 9,
-  //       "name": "jquery",
-  //       "img": "/img/jquery.png",
-  //       "component": "javascript",
-  //     },
-  //   ],
-  //   template: '<h1>{message}</h1>',
   // }
 });
 
 function setWindowHeight(){
     var wHeight = window.innerHeight;
-    document.body.style.height = wHeight + "px";
+    document.body.style.maxHeight = wHeight + "px";
 }
 
 window.addEventListener("resize",setWindowHeight,false);
